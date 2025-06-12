13343993641583bea46752f7de2b5607ac471ccb
David Slusser <dbslusser@gmail.com>
2025-06-12 16:24:01 -0700

    restore ingest_events for meetup

--- src/django_project/web/scripts/ingest_events.py
+++ src/django_project/web/scripts/ingest_events.py
@@ -1,21 +1,21 @@
 import time
 
+from django.db.models.manager import BaseManager
 from web.models import Event, TechGroup
 from web.tasks import ingest_future_eventbrite_events, ingest_future_meetup_events
 
-# def get_eventbright_events():
-#     # tech_group_list = TechGroup.objects.filter(enabled=True, platform__name="Eventbrite")
-#     tech_group_list = TechGroup.objects.filter(name__icontains="pod")
-#     for group in tech_group_list:
-#         job = ingest_future_eventbrite_events.s(group.pk)
-#         job.apply()
-#         time.sleep(1)
+
+def get_eventbright_events():
+    # tech_group_list = TechGroup.objects.filter(enabled=True, platform__name="Eventbrite")
+    tech_group_list = TechGroup.objects.filter(name__icontains="pod")
+    for group in tech_group_list:
+        job = ingest_future_eventbrite_events.s(group.pk)
+        job.apply()
+        time.sleep(1)
 
 
 def get_meetup_events() -> None:
-    # tech_group_list = TechGroup.objects.filter(enabled=True, name__icontains="python")
-    tech_group_list = TechGroup.objects.filter(pk=23)
-
+    tech_group_list = TechGroup.objects.filter(enabled=True, name__icontains="python")
     for group in tech_group_list:
         print("INFO: getting upcoming events for ", group.name)
         job = ingest_future_meetup_events.s(group.pk)
@@ -23,5 +23,5 @@ def get_meetup_events() -> None:
 
 
 def run():
-    # get_eventbright_events()
+    get_eventbright_events()
     get_meetup_events()
