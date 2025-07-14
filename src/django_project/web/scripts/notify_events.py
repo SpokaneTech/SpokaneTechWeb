a3d8ed27d701bb985dc62a779a00eae6fd1e2dfb
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-07-14 12:58:48 -0700

    Adds initial event message debug code.

---
+++ src/django_project/web/scripts/notify_events.py
@@ -0,0 +1,59 @@
+from django.utils import timezone
+from requests import post
+
+from django.conf import settings
+from web.models import Event
+
+
+
+
+# returns an array of events from the database
+def get_events():
+  # retrieve events from the database
+  events = Event.objects.filter(start_datetime__gte=timezone.now())
+
+  return events
+
+
+def get_events_as_json():
+  events = get_events()
+  output = []
+
+  # loop through events and generate json data for the output array
+  # todo: reformat this to make it more readable
+  for event in events:
+    output.append(f'{{"title": "{event.title}", "description": "{event.description}", "color": 0x00ff00, "fields": [{{"name": "Start Time", "value": "{event.start_datetime.strftime("%Y-%m-%d %H:%M:%S")}", "inline": False}}, {{"name": "End Time", "value": "{event.end_datetime.strftime("%Y-%m-%d %H:%M:%S")}", "inline": False}}]}}')
+
+  return output
+
+
+def notify_discord():
+  """Notify discord about upcoming events."""
+
+  discord_url = getattr(settings, 'DISCORD_WEBHOOK_URL', None)
+  if not discord_url:
+    print("Discord webhook URL not set")
+    return
+
+  events_json = get_events_as_json()
+  if not events_json:
+    print("No events to notify")
+    return
+
+  payload = json={
+    "content": "Upcoming events:",
+    "embeds": events_json
+  }
+
+  # send POST request to discord webhook url with events_json
+  response = post(discord_url, payload)
+
+  # debug: log the response
+  print(response.status_code)
+
+  # debug: log the response content
+  print(response.content)
+
+
+def run():
+  notify_discord()
