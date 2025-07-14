fa35267601c3cabc49aa9552be1a5eb6205cefaf
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-07-14 14:54:55 -0700

    Formats message to discord.

--- src/django_project/web/scripts/notify_events.py
+++ src/django_project/web/scripts/notify_events.py
@@ -1,8 +1,13 @@
 from django.utils import timezone
 from requests import post
+from bs4 import BeautifulSoup
 
 from django.conf import settings
 from web.models import Event
+
+def clean_html(text):
+    soup = BeautifulSoup(text, "html.parser")
+    return soup.get_text()
 
 
 # returns an array of events from the database
@@ -15,23 +20,30 @@ def get_events():
 
 # todo: this template needs cleanup.
 def get_event_object(event):
-    return {
-        "name": event.name,
-        "description": event.description, # todo: handle the html content and post to discord accordingly
-        "color": 0x00ff00,
-        "fields": [ # todo: clean up these start / end fields.
+    event_data = {
+        "title": event.name,
+        "description": clean_html(event.description),
+        "url": event.url,
+        "thumbnail": event.image.url if event.image else None,
+        "color": 0x00ff00, # todo: get custom colors from event host
+        "fields": [
             {
-                "name": "Start Time",
-                "value": event.start_datetime.strftime("%Y-%m-%d %H:%M:%S"),
-                "inline": False
+              "name": "When",
+              "value": f"{event.start_datetime.strftime('%A, %B %d %H:%M')}",
+              "inline": False
             },
             {
-                "name": "End Time",
-                "value": event.end_datetime.strftime("%Y-%m-%d %H:%M:%S"),
+                "name": "Where",
+                "value": f"**{event.location_name}**\n[{event.location_address}]({event.map_link})",
                 "inline": False
             }
-        ]
+        ],
+        "footer": {
+            "text": f"{event.group.name}"
+        }
     }
+
+    return event_data
 
 def get_events_as_object():
     events = get_events()
@@ -51,7 +63,8 @@ def notify_discord():
         return
 
     payload = {
-        "content": "Upcoming events:",
+        "username": "Event Notifier 🤖",
+        "content": "_Here are the upcoming Spokane Tech events for this week:_\n\n",
         "embeds": events
     }
 
