ffc2010c715d2f3b2f574304877b0a35c7daaa56
David Slusser <dbslusser@gmail.com>
2025-09-04 20:35:22 -0700

    adding discord notifier and webhook field to techgroup

--- src/django_project/web/signals.py
+++ src/django_project/web/signals.py
@@ -4,7 +4,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Event
-from .tasks import post_event_to_linkedin, post_event_to_spug_task
+from .tasks import (
+    post_event_to_discord,
+    post_event_to_linkedin,
+    post_event_to_spug_task,
+)
 
 
 @receiver(post_save, sender=Event)
@@ -13,10 +17,15 @@ def event_post_to_linkedin_signal(sender, instance, created, **kwargs) -> None:
     Signal receiver that triggers a Celery task to post the new event to LinkedIn.
     """
     if created:
-        job: Any = post_event_to_linkedin.s(instance.pk, is_new=True)
-        job.apply_async()
+        # post to Discord
+        discord_job: Any = post_event_to_discord.s(instance.pk, reminder=False)
+        discord_job.apply_async()
 
-        # if TechGroup is SPUG, also post to SPUG via api
+        # post to LinkedIn
+        linkedin_job: Any = post_event_to_linkedin.s(instance.pk, is_new=True)
+        linkedin_job.apply_async()
+
+        # if TechGroup is SPUG, also post to SPUG website via api
         if instance.group.name == "Spokane Python User Group":
-            job2 = post_event_to_spug_task.s(instance.pk)
-            job2.apply_async()
+            spug_job: Any = post_event_to_spug_task.s(instance.pk)
+            spug_job.apply_async()
