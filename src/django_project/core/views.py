e94d58f74c2cd82d81a475e503932f4427f26012
David Slusser <dbslusser@gmail.com>
2025-09-15 20:00:53 -0700

    adding host info page

--- src/django_project/core/views.py
+++ src/django_project/core/views.py
@@ -1,9 +1,15 @@
+import sys
+
+import django
+from django.conf import settings
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
 from django.views.decorators.http import require_GET
 
 
 @require_GET
-def robots_txt(request):
+def robots_txt(request) -> HttpResponse:
     return HttpResponse(robots_txt_content, content_type="text/plain")
 
 
@@ -13,3 +19,16 @@ Disallow: /__debug__/
 Disallow: /accounts/
 Disallow: /admin/
 """
+
+
+class HostView(View):
+    def get(self, request) -> HttpResponse:
+        host_info: dict[str, str] = {
+            "hostname": request.get_host(),
+            "remote_ip_address": request.META.get("REMOTE_ADDR", "Unknown"),
+            "user_agent": request.META.get("HTTP_USER_AGENT", "Unknown"),
+            "source_code": getattr(settings, "PROJECT_SOURCE", "Unknown"),
+            "python_version": f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}",
+            "django_version": django.get_version(),
+        }
+        return render(request, "core/host_info.html", host_info)
